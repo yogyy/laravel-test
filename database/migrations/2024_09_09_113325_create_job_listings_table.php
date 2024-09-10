9738571c53462d3b02806950a37a1e646a8d3461
yogyy <yogimr261@gmail.com>
2024-09-10 21:00:19 +0700

    Model Factories

--- database/migrations/2024_09_09_113325_create_job_listings_table.php
+++ database/migrations/2024_09_09_113325_create_job_listings_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Employer;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -15,6 +16,8 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->string('salary');
+            // $table->unsignedBigInteger('employer_id');
+            $table->foreignIdFor(Employer::class);
             $table->timestamps();
         });
     }
--- database/migrations/2024_09_10_134810_create_employers_table.php
+++ database/migrations/2024_09_10_134810_create_employers_table.php
@@ -11,10 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create('employers', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
-            $table->text('body');
+            $table->string('name');
             $table->timestamps();
         });
     }
@@ -24,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('posts');
+        Schema::dropIfExists('employers');
     }
 };
