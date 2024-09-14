fc0cf65df1dcd14cd0c25562c962fa6c17fe99c1
yogyy <yogimr261@gmail.com>
2024-09-14 20:16:47 +0700

    Route Resource

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,14 @@ Route::view('/about', 'about', [
     'nama' => 'Klein Moretti'
 ]);
 
-Route::controller(JobController::class)->group(function () {
-    Route::get('/jobs', 'index');
-    Route::post('/jobs', 'store');
-    Route::get('/jobs/create', 'create');
-    Route::get('/jobs/{job}', 'show');
-    Route::get('/jobs/{job}/edit', 'edit');
-    Route::patch('/jobs/{job}', 'update');
-    Route::delete('/jobs/{job}', 'destroy');
-});
+// Route::controller(JobController::class)->group(function () {
+//     Route::get('/jobs', 'index');
+//     Route::post('/jobs', 'store');
+//     Route::get('/jobs/create', 'create');
+//     Route::get('/jobs/{job}', 'show');
+//     Route::get('/jobs/{job}/edit', 'edit');
+//     Route::patch('/jobs/{job}', 'update');
+//     Route::delete('/jobs/{job}', 'destroy');
+// });
+
+Route::resource('jobs', JobController::class);
